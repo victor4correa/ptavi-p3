36de7f5856696f04c25f0a54c5e044ce3415441b
victor4correa <v.correar@alumnos.urjc.es>
2018-10-16 14:09:57 +0200

    karaoke.py terminado y pasado pep8

--- karaoke.py
+++ karaoke.py
@@ -11,44 +11,53 @@ from xml.sax import make_parser
 from smallsmilhandler import SmallSMILHandler
 from urllib.request import urlretrieve
 
-if __name__ == "__main__":
-    
-    try:
-        file_smil = sys.argv[1]
-        file_json = sys.argv[1].replace(".smil", ".json")
-        archivo_smil = []
+
+class KaraokeLocal():
+
+    def __init__(self, fich):
         parser = make_parser()
         cHandler = SmallSMILHandler()
         parser.setContentHandler(cHandler)
         parser.parse(open(file_smil))
-        
-        with open(file_json, "w") as fjson:
-        
-            listavalores = cHandler.get_tags()
-            for linea in listavalores:
-                lista_final = []
-                etiqueta = linea[0]
-                lista_final.append(etiqueta)
-                for atributo, valor in linea[1].items():
-                    lista_final.append (atributo + "=" + '"' + valor + '"')   
-                lista_final = '\t'.join(lista_final)
-                archivo_smil.append(lista_final + '\n')
-                
-            for linea in listavalores:
-                etiqueta = linea[0]
+        self.my_file = cHandler.get_tags()
+
+    def __str__(self):
+        archivo_smil = []
+        for linea in self.my_file:
+            lista_final = []
+            etiqueta = linea[0]
+            lista_final.append(etiqueta)
+            for atributo, valor in linea[1].items():
+                lista_final.append(atributo + "=" + '"' + valor + '"')
+            lista_final = '\t'.join(lista_final)
+            archivo_smil.append(lista_final + '\n')
+        print(archivo_smil)
+
+    def to_json(self, fjson):
+        with open(file_json, "w"):
+            json.dumps(self.my_file)
+
+    def do_local(self):
+        for linea in self.my_file:
                 for atributo, valor in linea[1].items():
                     if valor[:7] == "http://":
                         name_local = valor.split('/')[-1]
                         urlretrieve(valor, name_local)
                         print("Descargando %s..." % valor)
-                        
-                
-        """fjson = json.dumps(archivo_smil)
-        print(fjson)"""
-        
-                
-                
-                
+
+
+if __name__ == "__main__":
+
+    try:
+        file_smil = sys.argv[1]
+        file_json = sys.argv[1].replace(".smil", ".json")
+        file_karaoke = KaraokeLocal(file_smil)
+        file_karaoke.__str__()
+        file_karaoke.to_json(file_json)
+        file_karaoke.do_local()
+        file_karaoke.to_json('file_json.json')
+        file_karaoke.__str__()
+
     except IndexError:
-        
+
         sys.exit("Usage:python3 karaoke.py file.smil")
